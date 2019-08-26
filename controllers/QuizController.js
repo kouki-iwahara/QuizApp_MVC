a8b14579a398cc5d7144e8e98948189d777a211b
岩原こうき <kouki.iwahara19860712@gmail.com>
2019-08-26 19:21:49 +0900

    コメントの追加

--- controllers/QuizController.js
+++ controllers/QuizController.js
@@ -1,5 +1,6 @@
 const Quiz = require('../models/Quiz')
 
+// クイズデータをjsonにしてroutesに渡す
 module.exports.getQuizData = async (req, res) => {
   const quizData = await Quiz.createQuizData();
   return res.json(quizData);
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const quizController = require('../controllers/QuizController')
+const quizController = require('../controllers/QuizController');
 
-router.get('/', quizController.getQuizData)
+// クイズデータ参照のページ
+router.get('/', quizController.getQuizData);
 
 module.exports = router;
