120a40bf1c30d81aaa4445590eea14a510c34b71
岩原こうき <kouki.iwahara19860712@gmail.com>
2019-09-01 14:05:58 +0900

    ファイル名変更

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const quizController = require('../controllers/QuizController');
+const quizController = require('../controllers/quizController');
 
 // クイズデータ参照のページ
 router.get('/quiz', quizController.getQuizData);
